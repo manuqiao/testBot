a7ba009b790a77ed04b83dc699813ddb360eb00c
manuqiao <manuqiao@me.com>
2013-12-26 14:30:23 +0800

    Initial Commit

---
+++ testBot/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testBot
+//
+//  Created by qiaorujia on 12/26/13.
+//  Copyright (c) 2013 Eisoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
